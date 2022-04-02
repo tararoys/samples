60ab96b7d04f70c66515faf42d7cbf4fe65fcb30
tararoys <tlroys@gmail.com>
2022-04-02 08:44:12 -0500

    Update main.js

--- src/content/devices/input-output/js/main.js
+++ src/content/devices/input-output/js/main.js
@@ -93,17 +93,17 @@ function start() {
       track.stop();
     });
   }
-  const audioSource = audioInputSelect.value;
+  //const audioSource = audioInputSelect.value;
   const videoSource = videoSelect.value;
   const constraints = {
-    audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
+    //audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
     video: {deviceId: videoSource ? {exact: videoSource} : undefined}
   };
   navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
 }
 
-audioInputSelect.onchange = start;
-audioOutputSelect.onchange = changeAudioDestination;
+//audioInputSelect.onchange = start;
+//audioOutputSelect.onchange = changeAudioDestination;
 
 videoSelect.onchange = start;
 
